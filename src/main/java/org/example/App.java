ccbed13f3782d0c775f43090e15ecb4712077d51
hteteaindrayoo2034 <hteteaindrayoo30@gmail.com>
2025-09-29 14:10:20 +0630

    Extract employees
    Signed-off-by: hteteaindrayoo2034 <hteteaindrayoo30@gmail.com>

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,15 @@ import org.bson.Document;
 import java.sql.*;
 public class App
 {
-    public static void main(String[] args)
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -20,9 +28,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -33,9 +39,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -48,7 +51,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -62,4 +71,18 @@ public class App
             }
         }
     }
+
+
+        public static void main(String[] args)
+        {
+            // Create new Application
+            App a = new App();
+
+            // Connect to database
+            a.connect();
+
+            // Disconnect from database
+            a.disconnect();
+        }
+
 }
